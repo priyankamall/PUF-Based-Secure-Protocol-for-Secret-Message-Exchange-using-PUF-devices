0f91f9c5011ec2d9aa3d313b5e15c8c7f59421b5
Priyanka Mall <87891538+PMall09@users.noreply.github.com>
2021-09-05 22:49:31 +0530

    Update Code1.py

--- Code1.py
+++ Code1.py
@@ -32,7 +32,7 @@ macro Rc'=ADD(Ras,Ras-c);
 macro Hc'=H(IDc,IDu,Ac',BFIu',Rc',T2);
 match(Hc', Hc);
 macro SKe-v = H(IDc,IDu,Ras,Rc );
-claim_User(User,Niagree);
+claim_User(User,Niagree);#non-injective Agreement
 claim_User(User, Nisynch);
 claim_User(User,Secret,PWu);
 claim_User(User,Secret, SKe-v);
