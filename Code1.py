e77dfdcd6061e0d886981c8fce9f9c2bf233a299
Priyanka Mall <87891538+priyankamall@users.noreply.github.com>
2021-10-02 21:48:44 +0530

    Update Code1.py

--- Code1.py
+++ Code1.py
@@ -34,7 +34,7 @@ macro Hc'=H(IDc,IDu,Ac',BFIu',Rc',T2);
 match(Hc', Hc);
 macro SKe-v = H(IDc,IDu,Ras,Rc );
 claim_User(User,Niagree); #non-injective Agreement
-claim_User(User, Nisynch);#non-injective Synchronization
+claim_User(User, Nisynch); #non-injective Synchronization
 claim_User(User,Secret,PWu);#verify the user password secrecy
 claim_User(User,Secret, SKe-v);#verify the user and cloud server session key secrecy
 }
